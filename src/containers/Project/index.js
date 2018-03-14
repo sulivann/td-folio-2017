7c830138463e8e949607124969af8dc3b9fa7496
Clément Vion <clement.vion49@gmail.com>
2018-03-14 11:30:03 +0100

    Return to normal scroll

--- src/containers/Project/index.js
+++ src/containers/Project/index.js
@@ -192,33 +192,33 @@ export default Vue.extend({
     },
 
     smoothScroll() {
-      let section = document.querySelector('.project');
-      let targetY = 0;
+      // let section = document.querySelector('.project');
+      // let targetY = 0;
 
-      this.scroll.on((e) => {
-        targetY += e.deltaY;
-        targetY = Math.max( (section.scrollHeight - window.innerHeight) * -1, targetY);
-        targetY = Math.min(0, targetY);
+      this.scroll.on(() => {
+      //   targetY += e.deltaY;
+      //   targetY = Math.max( (section.scrollHeight - window.innerHeight) * -1, targetY);
+      //   targetY = Math.min(0, targetY);
 
         this.handleScrollDown();
         this.handleBackToHome();
       });
 
-      let currentY = 0;
-      this.scrollEasing = 0.04;
+      // let currentY = 0;
+      // this.scrollEasing = 0.07;
+      //
+      // let run = () => {
+      //   this.request = requestAnimationFrame(run);
+      //   currentY += (targetY - currentY) * this.scrollEasing;
+      //   let t = 'translateY(' + currentY + 'px) translateZ(0)';
+      //   let s = section.style;
+      //   s["transform"] = t;
+      //   s["webkitTransform"] = t;
+      //   s["mozTransform"] = t;
+      //   s["msTransform"] = t;
+      // };
 
-      let run = () => {
-        this.request = requestAnimationFrame(run);
-        currentY += (targetY - currentY) * this.scrollEasing;
-        let t = 'translateY(' + currentY + 'px) translateZ(0)';
-        let s = section.style;
-        s["transform"] = t;
-        s["webkitTransform"] = t;
-        s["mozTransform"] = t;
-        s["msTransform"] = t;
-      };
-
-      run();
+      // run();
     },
 
     returnNextProject() {
